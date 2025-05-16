ffc2663cfda4f1effa5aeab0d29bfa577079ce22
dmitriy <ytdimagold752@gmail.com>
2025-05-16 10:21:19 +0500

    add change password

--- app/handlers.py
+++ app/handlers.py
@@ -5,9 +5,8 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 
 import sqlite3
-from pprint import pprint
 
-from app.keyboard import edit_kb, in_db, not_in_db, show_pass, hide_pass
+from app.keyboard import edit_kb, in_db, not_in_db, show_pass, hide_pass, cancel_kb
 
 
 router = Router()
@@ -18,7 +17,9 @@ class Registration(StatesGroup):
 
 class EditInfo(StatesGroup):
     editing_login = State()
+    text_old_password = State()
     editing_password = State()
+    edit_profile_pic = State()
 
 
 @router.message(CommandStart())
@@ -31,10 +32,10 @@ async def start_message(message: Message):
     login TEXT NOT NULL,
     password TEXT NOT NULL)""")
     cr.execute(f"""SELECT * FROM Users WHERE id = {message.from_user.id}""")
-    if cr.fetchall():
-        await message.answer('Вы уже есть в БД ✅. Можете удалить или изменить данные', reply_markup=in_db)
+    if cr.fetchone():
+        await message.answer('Вы уже есть в БД ✅. Можете удалить аккаунт или изменить данные', reply_markup=in_db)
     else:
-        await message.answer('Приветствую. Здесь вы можете зарегистрироваться в БД', reply_markup=not_in_db)
+        await message.answer('Приветствую! Здесь вы можете зарегистрироваться в супер технологичную и безопасную базу данных', reply_markup=not_in_db)
 
     con.commit()
     con.close()
@@ -45,24 +46,29 @@ async def start_reg(message: Message, state: FSMContext):
     con = sqlite3.connect('data.db')
     cr = con.cursor()
     cr.execute(f"SELECT * FROM Users WHERE id = '{message.from_user.id}'")
-    if cr.fetchall():
+    if cr.fetchone():
         await message.answer('Вы уже зарегистрированы ✅')
     else:
-        await message.answer('Отлично! Пожалуйста введите ваш логин: ')
+        await message.answer('Отлично! Пожалуйста введите ваш логин: ', reply_markup=cancel_kb)
         await state.set_state(Registration.waiting_for_login)
 
 
 @router.message(Registration.waiting_for_login)
 async def get_name(message: Message, state: FSMContext):
+    if message.text == 'Отмена ❌':
+        await message.answer('Регистрация отменена ✅')
+        await state.clear()
+        return
+
     con = sqlite3.connect('data.db')
     cr = con.cursor()
     cr.execute(f"SELECT * FROM Users WHERE login = '{message.text}'")
-    if not cr.fetchall():
+    if not cr.fetchone():
         await state.update_data(login=message.text)
         await state.set_state(Registration.waiting_for_password)
-        await message.answer('Отлично! Теперь введите пароль: ')
+        await message.answer('Отлично! Теперь введите пароль: ', reply_markup=cancel_kb)
     else:
-        await message.answer('Такой пользователь уже существует. Пожалуйста, введите другой логин: ')
+        await message.answer('Такой пользователь уже существует. Пожалуйста, введите другой логин: ', reply_markup=cancel_kb)
 
     con.commit()
     con.close()
@@ -70,12 +76,17 @@ async def get_name(message: Message, state: FSMContext):
 
 @router.message(Registration.waiting_for_password)
 async def get_password(message: Message, state: FSMContext):
+    if message.text == 'Отмена ❌':
+        await message.answer('Регистрация отменена ✅')
+        await state.clear()
+        return
+
     await state.update_data(password=message.text)
     data = await state.get_data()
     con = sqlite3.connect('data.db')
     cr = con.cursor()
 
-    cr.execute(f"INSERT INTO Users VALUES ({int(message.from_user.id)}, '{data['login']}', '{data['password']}')")
+    cr.execute(f"INSERT INTO Users VALUES ({message.from_user.id}, '{data['login']}', '{data['password']}')")
 
     con.commit()
     con.close()
@@ -90,14 +101,15 @@ async def check_profile(message: Message):
     cr = con.cursor()
 
     cr.execute(f"SELECT * FROM Users WHERE id = {message.from_user.id}")
-    data = cr.fetchall()
+    data = cr.fetchone()
     if data:
-        await message.answer(f'Ваш логин: {data[0][1]}\nВаш пароль: {'*' * len(data[0][2])}', reply_markup=show_pass)
+        await message.answer(f'Ваш логин: {data[1]}\nВаш пароль: {'*' * len(data[2])}', reply_markup=show_pass)
     else:
         await message.answer('Вы ещё не зарегистрированы в БД', reply_markup=not_in_db)
 
     con.commit()
     cr.close()
+
 
 @router.callback_query(F.data == 'showpass')
 async def show_password(callback: CallbackQuery):
@@ -105,10 +117,10 @@ async def show_password(callback: CallbackQuery):
     cr = con.cursor()
 
     cr.execute(f"SELECT * FROM Users WHERE id = {callback.from_user.id}")
-    data = cr.fetchall()
+    data = cr.fetchone()
     if data:
         await callback.answer('Показать пароль')
-        await callback.message.edit_text(f'Ваш логин: {data[0][1]}\nВаш пароль: {data[0][2]}', reply_markup=hide_pass)
+        await callback.message.edit_text(f'Ваш логин: {data[1]}\nВаш пароль: {data[2]}', reply_markup=hide_pass)
     else:
         await callback.answer('Вы не зарегистрированы')
         await callback.message.answer('Вы ещё не зарегистрированы в БД', reply_markup=not_in_db)
@@ -123,10 +135,10 @@ async def hide_password(callback: CallbackQuery):
     cr = con.cursor()
 
     cr.execute(f"SELECT * FROM Users WHERE id = {callback.from_user.id}")
-    data = cr.fetchall()
+    data = cr.fetchone()
     if data:
         await callback.answer('Скрыть пароль')
-        await callback.message.edit_text(f'Ваш логин: {data[0][1]}\nВаш пароль: {'*' * len(data[0][2])}', reply_markup=show_pass)
+        await callback.message.edit_text(f'Ваш логин: {data[1]}\nВаш пароль: {'*' * len(data[2])}', reply_markup=show_pass)
     else:
         await callback.answer('Вы не зарегистрированы')
         await callback.message.answer('Вы ещё не зарегистрированы в БД', reply_markup=not_in_db)
@@ -140,13 +152,13 @@ async def del_acc(message: Message):
     con = sqlite3.connect('data.db')
     cr = con.cursor()
 
-    cr.execute(f'SELECT *  FROM Users WHERE id = {int(message.from_user.id)}')
+    cr.execute(f'SELECT * FROM Users WHERE id = {message.from_user.id}')
 
-    if cr.fetchall():
-        cr.execute(f'DELETE FROM Users WHERE id = {int(message.from_user.id)}')
+    if cr.fetchone():
+        cr.execute(f'DELETE FROM Users WHERE id = {message.from_user.id}')
         con.commit()
         con.close()
-        await message.answer('Аккаунт удалён')
+        await message.answer('Аккаунт удалён ✅')
     else:
         await message.answer('Вы ещё не зарегистрированы в БД', reply_markup=not_in_db)
 
@@ -157,10 +169,13 @@ async def edit_acc(message: Message):
     cr = con.cursor()
 
     cr.execute(f'SELECT * FROM Users WHERE id = {message.from_user.id}')
-    if not cr.fetchall():
+    if not cr.fetchone():
         await message.answer('Вы ещё не зарегистрированы в БД', reply_markup=not_in_db)
     else:
         await message.answer('Отлично! Выберите, что хотите изменить:', reply_markup=edit_kb)
+
+    con.commit()
+    con.close()
 
 
 @router.callback_query(F.data == 'editlogin')
@@ -169,42 +184,121 @@ async def change_login(callback: CallbackQuery, state: FSMContext):
     cr = con.cursor()
 
     cr.execute(f"SELECT login FROM Users WHERE id = {callback.from_user.id}")
-    if cr.fetchall():
+    if cr.fetchone():
         await callback.answer('Вы выбрали изменить логин ✅')
-        await callback.message.answer('Введите новый логин:')
+        await callback.message.answer('Введите новый логин:', reply_markup=cancel_kb)
         await state.set_state(EditInfo.editing_login)
     else:
         await callback.message.answer('Вы ещё не зарегистрированы в БД', reply_markup=not_in_db)
 
+    con.commit()
+    con.close()
+
 
 @router.callback_query(F.data == 'editpassword')
 async def change_password(callback: CallbackQuery, state: FSMContext):
-    await callback.answer('Вы выбрали изменить пароль ✅')
-    await state.set_state(EditInfo.editing_password)
+    con = sqlite3.connect('data.db')
+    cr = con.cursor()
+
+    cr.execute(f"SELECT login FROM Users WHERE id = {callback.from_user.id}")
+    if cr.fetchone():
+        await callback.answer('Вы выбрали изменить пароль ✅')
+        await callback.message.answer('Сначала введите старый пароль:', reply_markup=cancel_kb)
+        await state.set_state(EditInfo.text_old_password)
+    else:
+        await callback.message.answer('Вы ещё не зарегистрированы в БД', reply_markup=not_in_db)
+
+    con.commit()
+    con.close()
+
+
+@router.callback_query(F.data == 'canceledit')
+async def cancel_edit(callback: CallbackQuery, state: FSMContext):
+    await callback.answer('Вы выбрали отмену')
+    await callback.message.answer('Хорошо ✅')
+    await callback.message.delete()
+    await state.clear()
 
 
 @router.message(EditInfo.editing_login)
 async def edit_login(message: Message, state: FSMContext):
+    if message.text == 'Отмена ❌':
+        await message.answer('Хорошо ✅')
+        await state.clear()
+        return
+
     con = sqlite3.connect('data.db')
     cr = con.cursor()
 
     cr.execute(f"SELECT login FROM Users WHERE id = {message.from_user.id}")
-    data = cr.fetchall()
+    data = cr.fetchone()
     if not data:
         await message.answer('Вы ещё не зарегистрированы в БД', reply_markup=not_in_db)
-    elif data[0][0] == message.text:
-        await message.answer('Вы ввели свой же логин, пожалуйста введите другой')
+    elif data[0] == message.text:
+        await message.answer('Вы ввели свой же логин, пожалуйста, введите другой:', reply_markup=cancel_kb)
     else:
         cr.execute(f"SELECT * FROM Users WHERE login = '{message.text}'")
-        if cr.fetchall():
+        if cr.fetchone():
             await message.answer('Такой пользователь уже существует. Пожалуйста, введите другой логин:', reply_markup=in_db)
         else:
             cr.execute(f"UPDATE Users SET login = '{message.text}' WHERE id = {int(message.from_user.id)}")
             await state.clear()
-            await message.answer(f'Логин изменён ✅\nНовый логин: {message.text}', reply_markup=in_db)
+            await message.answer(f'Логин успешно изменён ✅\nНовый логин: {message.text}', reply_markup=in_db)
 
     con.commit()
     con.close()
+
+
+@router.message(EditInfo.text_old_password)
+async def wait_for_old_password(message: Message, state: FSMContext):
+    if message.text == 'Отмена ❌':
+        await message.answer('Хорошо ✅')
+        await state.clear()
+        return
+
+    con = sqlite3.connect('data.db')
+    cr = con.cursor()
+
+    cr.execute(f"SELECT password FROM Users WHERE id = {message.from_user.id}")
+    data = cr.fetchone()
+    if data and data[0] == message.text:
+        await message.answer('Отлично! Вы ввели верный пароль\nВведите новый пароль:', reply_markup=cancel_kb)
+        await state.set_state(EditInfo.editing_password)
+    else:
+        await message.answer('Пароль неверный. Пожалуйста, попробуйте снова', reply_markup=cancel_kb)
+
+    con.commit()
+    con.close()
+
+
+@router.message(EditInfo.editing_password)
+async def editing_password(message: Message, state: FSMContext):
+    if message.text == 'Отмена ❌':
+        await message.answer('Хорошо ✅')
+        await state.clear()
+        return
+
+    con = sqlite3.connect('data.db')
+    cr = con.cursor()
+
+    cr.execute(f"SELECT * FROM Users WHERE id = {message.from_user.id}")
+    data = cr.fetchone()
+
+    if not data:
+        await message.answer('Вы ещё не зарегистрированы в БД', reply_markup=not_in_db)
+    else:
+        cr.execute(f"UPDATE Users SET password = '{message.text}' WHERE id = {message.from_user.id}")
+        await message.answer(f'Пароль успешно изменён ✅\nНовый пароль: {message.text}', reply_markup=in_db)
+        await state.clear()
+
+    con.commit()
+    con.close()
+
+
+@router.message(F.text == 'Добавить/изменить аватарку профиля')
+async def set_profile_pic(message: Message, state: FSMContext):
+    await message.answer('Отправьте фотографию в чат:')
+    await state.set_state(EditInfo.edit_profile_pic)
 
 
 @router.message(Command('all'), F.from_user.id == 1999317423)
@@ -226,5 +320,5 @@ async def list_of_users(message: Message):
 
 
 @router.message()
-async def ya_blat_ne_ponimau(message: Message):
+async def cant_understand(message: Message):
     await start_message(message)
--- app/keyboard.py
+++ app/keyboard.py
@@ -9,7 +9,8 @@ def create_markup(buttons):
     return markup.adjust(2).as_markup(resize_keyboard=True)
 
 edit_kb = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Логин', callback_data='editlogin'), InlineKeyboardButton(text='Пароль', callback_data='editpassword')]
+    [InlineKeyboardButton(text='Логин', callback_data='editlogin'), InlineKeyboardButton(text='Пароль', callback_data='editpassword')],
+    [InlineKeyboardButton(text='Отмена ❌', callback_data='canceledit')]
 ])
 
 show_pass = InlineKeyboardMarkup(inline_keyboard=[
@@ -20,5 +21,6 @@ hide_pass = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Скрыть пароль', callback_data='hidepass')]
 ])
 
-in_db = create_markup(['Удалить аккаунт', 'Изменить данные', 'Мой профиль'])
+in_db = create_markup(['Удалить аккаунт', 'Изменить данные', 'Мой профиль', 'Добавить/изменить аватарку профиля'])
 not_in_db = create_markup(['Зарегистрироваться в БД'])
+cancel_kb = create_markup(['Отмена ❌'])
